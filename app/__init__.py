5f0d985092dfa4ed9c4391fd683e2a4376564a01
Tsepo Matheleli <tsepo.matheleli@umuzi.org>
2024-09-13 12:10:17 +0200

    Created a basic file structure for we applications.

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from app.routes.main_routes import main
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configuration
+    app.config.from_pyfile('../config.py')
+
+    # Register blueprints
+    app.register_blueprint(main)
+
+    return app
